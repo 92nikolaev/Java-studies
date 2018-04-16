f81bd16c7ad2a1a7076093fd70920b8d94bb5cb7
Yaroslav S <shukevich@users.noreply.github.com>
2018-04-16 20:25:33 +0300

    Add files via upload

---
+++ DB/SQL/database-drop.sql
@@ -0,0 +1,18 @@
+-- Drop tables
+drop table if exists eShop.user_data;
+drop table if exists eShop.user;
+-- Delete database
+drop database if exists eShop;
+
+/*
+RESERVED
+-- Drop tables
+drop table if exists eshop_database.user_data;
+drop table if exists eshop_database.user;
+-- Delete database
+drop database if exists eshop_database;
+
+
+
+*/
+
