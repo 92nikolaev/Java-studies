8e85c8c24ff8afdea0321189200652dae38f612f
Yaroslav S <shukevich@users.noreply.github.com>
2018-04-28 22:41:01 +0300

    Add files via upload

---
+++ E-Commerce/Product.java
@@ -0,0 +1,53 @@
+package by.it.academy;
+
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Product {
+    private Calendar createdDate;
+    private String description;
+    private int id;
+    private String name;
+    private double price;
+
+    public Calendar getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Calendar createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
